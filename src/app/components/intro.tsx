3c79cc3cd03643ece1c6020fd806c0d79a102171
wikidotexe <07wikiarlianm@gmail.com>
2024-12-29 16:30:46 +0700

    first commit

---
+++ src/app/components/intro.tsx
@@ -0,0 +1,50 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
+import { Mail } from "lucide-react";
+
+// Animation
+import { Fade } from "react-awesome-reveal";
+import { motion } from "framer-motion";
+
+export default function Intro() {
+  return (
+    <section id="home" className="mb-28 max-w-[75rem] text-center sm:mb-0">
+      <div className="flex item-center justify-center">
+        <div className="relative">
+          <motion.div initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ type: "spring", stiffness: 125, damping: 10, duration: 0.2 }}>
+            <Image src="/images/hero-image.png" width="400" height="400" alt="portrait" quality="100" priority={true} className="rounded-full object-cover shadow-xl" />
+          </motion.div>
+        </div>
+      </div>
+      <Fade direction="up" delay={400} cascade damping={1e-1} triggerOnce={true}>
+        <h1 className="mb-10 mt-4 px-4 text-2xl sm:text-4xl">
+          <span className="font-medium !leading-[1.5]">Hello, this is my personal portfolio website 👋</span>
+          <p className="text-[14px]">My journey in the realm of technology has been marked by diverse experience in IT Support</p>
+        </h1>
+      </Fade>
+      <motion.div className="flex sm:flex-row items-center justify-center gap-4 px-4 text-lg font-medium" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.1 }}>
+        <Link href="#" className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 dark:bg-white/10 active:scale-105 transition">
+          Connect <Mail color={"#9ca3af"} />
+        </Link>
+        <a
+          className="bg-gray-900 p-4 text-white flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://www.linkedin.com/in/dwiki-arlian-maulana-852b14209/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          className="bg-gray-900 p-4 text-white flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com/wikidotexe"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,19 @@
+import { Sora } from "next/font/google";
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        Sora: [`var(--font-sora)`, `sans-serif`],
       },
     },
   },
   plugins: [],
+  darkMode: "class",
 } satisfies Config;
