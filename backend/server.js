fe00bee2061737ae89ef3fa9a576e970a87e9045
wikidotexe <07wikiarlianm@gmail.com>
2024-12-31 13:50:15 +0700

    Updated

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config(); // Memuat variabel dari file .env
+Require("dotenv").config(); // Make sure to load .env
 
 const express = require("express");
 const bodyParser = require("body-parser");
