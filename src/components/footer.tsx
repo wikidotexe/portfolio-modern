2ca82288e8ac1525876b97a470d25e16e37b6117
wikidotedxe <07wikiarlianm@gmail.com>
2025-03-19 12:37:31 +0700

    edit footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="mb-5 px-4 text-center text-gray-500">
-      <small className="mb-2 block text-xs">&copy; 2024 Wikidotexe . All rights reserved.</small>
+      <small className="mb-2 block text-xs">&copy; 2024-2025 Wikidotexe . All rights reserved.</small>
     </footer>
   );
 }
